1dc9fe414254e163b2a1a83af1e6f81f14a8ff25
Santhanakrishnan Mariappan <81145004+santhanakrishnan-Mariappan@users.noreply.github.com>
2021-04-17 19:15:51 +0530

    Add files via upload

---
+++ learningjs.js
@@ -0,0 +1,14 @@
+function view(num) {
+    document.getElementById("result").value += num;
+}
+
+function compute() {
+    var x = document.getElementById("result").value;
+    var y = eval(x);
+    document.getElementById("result").value = y
+}
+
+function clr() {
+    document.getElementById("result").value = " ";
+}
+
